8540d7757c21bd25a24e451454faf8b6bc6af914
server_42 <devbhattacharya42@gmail.com>
2023-11-12 11:10:09 +0530

    react sort users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
-// import data from db.json
+import { useState } from "react";
+import UserDetails from "./components/UserDetails";
 
 function App() {
- 
+ const [users,setUsers] = useState([])
+
+ const usersDisplay = async() => {
+    try{
+      const res = await fetch('http://localhost:8080/users')
+      const data = await res.json()
+      setUsers(data)
+      console.log(data)
+    }catch(e){
+      console.log(e)
+    }
+ }
 
   return (
     <div className="App" data-testid = 'app'>
+      <button onClick={() => usersDisplay()}>Click</button>
       <button  data-testid = 'sort-asc-btn'>Sort by Asc</button>
       <button  data-testid = 'sort-desc-btn'>Sort by Desc</button>
       {/*  map through the users data and pass props to the Userdetails component */}
+      {users.map((user) => (
+        <UserDetails key = {user.id} {...user} />
+      ))}
     </div>
   );
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,28 +1,31 @@
 // import module.css here;
-const UserDetails = (props) => {
+import styles from './userDetails.module.css'
+
+const UserDetails = ({avatar,first_name,last_name,address,karma,followers,posts,is_following}) => {
   return (
     <>
       <div data-testid="user-container">
         {/* user image */}
+        <img src={avatar} alt="userImage" />
         <div>
           <div>
-            <h3 data-testid="user-fname"></h3>
-            <h3 data-testid="user-lname"></h3>
+            <h3 data-testid="user-fname">{first_name}</h3>
+            <h3 data-testid="user-lname">{last_name}</h3>
           </div>
           <div>
-            <p data-testid="user-address"></p>
+            <p data-testid="user-address">{address}</p>
           </div>
         </div>
         <div>
-          <h3 data-testid="user-karma"></h3>
+          <h3 data-testid="user-karma">{karma}</h3>
         </div>
         <div>
-          <h3 data-testid="user-followers"></h3>
+          <h3 data-testid="user-followers">{followers}</h3>
         </div>
         <div>
-          <h3 data-testid="user-posts"></h3>
+          <h3 data-testid="user-posts">{posts}</h3>
         </div>
-        <button data-testid="follow-btn"></button>
+        {is_following ? <button data-testid="follow-btn">follow</button> : <button data-testid="follow-btn">unfollow</button>}
       </div>
     </>
   );
